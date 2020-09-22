92258cde182597d58c476e32c30774b81875b51f
Agustina Prats <59830072+agusprats@users.noreply.github.com>
2020-09-22 09:45:19 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-/*!
-    * Start Bootstrap - Agency v6.0.1 (https://startbootstrap.com/template-overviews/agency)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
-    */
+
     (function ($) {
     "use strict"; // Start of use strict
 
